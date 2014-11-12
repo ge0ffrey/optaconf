5876516e2d1431964787312b75d0f37617fb8c63
Dave Hughes <djhughes@gmail.com>
2014-11-12 09:56:53 +0100

    Added import method to Schedule resource

--- optaconf-webapp/src/main/webapp/src/dashboard/dashboard.js
+++ optaconf-webapp/src/main/webapp/src/dashboard/dashboard.js
@@ -4,11 +4,20 @@ angular.module('dashboard', [])
         vm.title = 'Dashboard';
 
         vm.import = function() {
-            Schedule.get({});
+            console.log("Importing devoxx schedule...");
+            Schedule.import(function() {
+                console.log('imported devoxx schedule!');
+            });
         }
     }])
     .factory('Schedule', ['$resource', '$window', function($resource, $window) {
         var contextPath = $window.location.pathname.substr(1).split('/')[0];
 
-        return $resource("http://localhost:8080/" + contextPath + "/rest/123/schedule/import/devoxx");
+        return $resource("http://localhost:8080/" + contextPath + "/rest/123/schedule/import/devoxx", null, {
+            'import': {
+                method: 'GET',
+                responseType: 'text',
+                transformResponse: []
+            }
+        });
     }]);;
