70269c2d9541bb86ca47b4247368a93e9671b599
Matyas Danter <mdanter@gmail.com>
2015-11-25 08:39:56 -0700

    cleanup and solver termination is now 25 seconds

--- optaconf-webapp/src/main/java/org/optaconf/service/TalkService.java
+++ optaconf-webapp/src/main/java/org/optaconf/service/TalkService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -98,6 +99,7 @@ public class TalkService {
         }
         return dayTimeslotRoomToTalkMap;
     }
+
 
     @GET
     @Path("/schedule")
